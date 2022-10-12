832d2c9024ec328fdaea327b43691299d4027522
Alex <lex.courtney18@gmail.com>
2022-10-12 12:38:42 -0500

    download code snapshot to try to fix bugs. Unsuccessful

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -19,16 +19,16 @@ const ThoughtList = ({ thoughts, title }) => {
                 className="text-light"
               >
                 {thought.username}
-              </Link>
+              </Link>{' '}
               thought on {thought.createdAt}
             </p>
             <div className="card-body">
               <Link to={`/thought/${thought._id}`}>
-              <p>{thought.thoughtText}</p>
-              <p className="mb-0">
-                Reactions: {thought.reactionCount} || Click to{' '}
-                {thought.reactionCount ? 'see' : 'start'} the discussion!
-              </p>
+                <p>{thought.thoughtText}</p>
+                <p className="mb-0">
+                  Reactions: {thought.reactionCount} || Click to{' '}
+                  {thought.reactionCount ? 'see' : 'start'} the discussion!
+                </p>
               </Link>
             </div>
           </div>
